0a28240547c918ee3f94f2c4251993b2973093be
marciohsilva <marciohsilva@yahoo.com.br>
2018-07-22 16:08:50 -0400

    Update do arquivo Test.py

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Escreva uma Mensagem para o Cabeçalho da Página.")
+        self.assertRegex(result.data.decode(), "Esta é minha primeira aplicação Devops - Marcio Henrique.")
 
 
 if __name__ == "__main__":
